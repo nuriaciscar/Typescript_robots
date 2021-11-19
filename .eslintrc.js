389d566d7dc401133832b18d86d4fc57245dc01a
Nuria Ciscar <nuriaciscar@MacBook-Pro-de-MacBook.local>
2021-11-19 13:36:46 +0100

    Initial files

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ["airbnb-base", "airbnb - typrscript / base", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "no-param-reassign": "off",
+    "no-plusplus": "off",
+    "no-restricted-syntax": "off",
+    "consistent-return": "off",
+    "no-debugger": "off",
+    "no-console": "off",
+  },
+};
